be0250dc69d0893207c020b5ffce9c6765e73f3a
Tautology <tautology@cinemation.live>
2021-01-02 16:18:13 -0500

    Fixed alt+w toggling cinema mode

--- src/js/legacy/external_hotkeys.js
+++ src/js/legacy/external_hotkeys.js
@@ -102,10 +102,10 @@ if (!window[CHANNEL.name]) window[CHANNEL.name] = {};
     if (!event.shiftKey && !event.ctrlKey && event.altKey) {
       if (
         event.which === KEYCODE.W &&
-        $("#cinematoggle").length &&
+        $("#fw-leftcontrols-toggle").length &&
         !(localStorage.getItem(`BrazilianKeyboard`) === "true")
       ) {
-        $("#cinematoggle").click();
+        $("#fw-leftcontrols-toggle").click();
         event.preventDefault();
         return false;
       }
